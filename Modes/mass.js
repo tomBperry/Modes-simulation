32fa56c794849acd04cc45a95dada2e69c573113
tomBperry <40521601+tomBperry@users.noreply.github.com>
2018-07-05 15:06:15 +0100

    Update mass.js

--- Modes/mass.js
+++ Modes/mass.js
@@ -53,6 +53,7 @@ function mousey() {
     dy2 = dy2 * dy2;
     if (dx2 + dy2 < balls[i].radius * balls[i].radius) {
       balls.position.y = mouseY;
+      console.log("Ball: " + i + "is being dragged");
     }
   }
 }
